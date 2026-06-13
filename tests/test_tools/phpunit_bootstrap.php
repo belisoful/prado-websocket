91bd1a99a548c1c1e586593a0ba779b5cca06de2
Belisoful <belisoful@icloud.com>
2026-06-13 00:58:56 +0000

    php-cs-fixer, phpstan, phpunit config and bootstrap

---
+++ tests/test_tools/phpunit_bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Common settings for all unit tests of the PRADO WebSockets extension.
+ *
+ * Registers the extension's error message file so exception codes resolve, then
+ * autoloads the framework and the extension via Composer's PSR-4 map.
+ */
+
+require_once(__DIR__ . '/../../vendor/autoload.php');
+
+\Prado\Exceptions\TException::addMessageFile(__DIR__ . '/../../src/IO/Socket/WebSocket/errorMessages.txt');
